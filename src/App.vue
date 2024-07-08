34d7178a4bf9815d68bf8da0ef08a83c7df187ec
Majid Khan <luthfullahhilmajid172@gmail.com>
2024-07-08 23:11:21 +0600

    installation

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h4>hello</h4>
+  </div>
+</template>
+
+<style scoped>
+</style>
